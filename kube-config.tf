d477017a660b3d213d538ffb5eb40ba56055ebe3
Avinash Yadav <avinash.guruka@gmail.com>
2023-04-06 11:07:35 +0530

    updating tf-module-eks

--- kube-config.tf
+++ kube-config.tf
@@ -1,7 +1,7 @@
 resource "null_resource" "get-kube-config" {
   depends_on = [aws_eks_node_group.node-group]
   provisioner "local-exec" {
-    command = "aws eks update-kubeconfig  --name ${var.ENV}-eks-cluster"
+    command = "aws eks update-kubeconfig --region us-east-1 --name ${var.ENV}-eks-cluster"
   }
 }
 
